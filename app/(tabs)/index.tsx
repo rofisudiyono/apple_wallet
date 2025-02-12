988374b8fd060756cc1554a9bbfa7a65e6e11247
Rofi Sudiyono <rofisudyono@gmail.com>
2025-02-12 21:49:17 +0800

    integration wallet manager

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,111 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import WalletManager from "react-native-wallet-manager";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const blobToDataUrl = async (blob: any) =>
+  new Promise((r) => {
+    let a = new FileReader();
+    a.onload = r;
+    a.readAsDataURL(blob);
+  }).then((e: any) => e.target.result);
 
-export default function HomeScreen() {
+export default function Index() {
+  const [name, setName] = useState<string | undefined>(undefined);
+  const [isLoadingPass, setIsLoadingPass] = useState(false);
+  const handleChangeText = (value: any) => {
+    setName(value);
+  };
+  const handleSubmit = async () => {
+    // Skip if the name is not set
+    if (!Boolean(name)) return;
+    try {
+      setIsLoadingPass(true);
+      const pass = await fetch(
+        "https://inherently-fancy-goblin.ngrok-free.app",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name,
+          }),
+        }
+      );
+      const passBlob = await pass.blob();
+      const responseAdd = await WalletManager.addPassFromUrl(
+        await blobToDataUrl(passBlob)
+      );
+      console.log(">", responseAdd);
+
+      setIsLoadingPass(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getData = async () => {
+    const pass = await fetch("https://inherently-fancy-goblin.ngrok-free.app", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(JSON.stringify(pass));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.labelContainer}>
+        <Text style={styles.label}>Enter your name below please</Text>
+        {isLoadingPass && <ActivityIndicator />}
+      </View>
+      <TextInput
+        editable={!isLoadingPass}
+        value={name}
+        onChangeText={handleChangeText}
+        style={styles.input}
+      />
+      <Button
+        disabled={isLoadingPass}
+        title="Get your pass now!"
+        onPress={handleSubmit}
+      />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    height: 40,
+    width: "60%",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  label: {
+    fontSize: 16,
+  },
+  labelContainer: {
+    alignSelf: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%",
+    height: 18,
   },
 });
